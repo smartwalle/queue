f9f16e805aea41b94919294dacc7a6dd0ac9456a
smartwalle <smartwalle@gmail.com>
2022-04-28 09:16:56 +0800

    测试

--- priority/queue_test.go
+++ priority/queue_test.go
@@ -2,15 +2,23 @@ package priority_test
 
 import (
 	"github.com/smartwalle/queue/priority"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func BenchmarkPriorityQueue_Enqueue(b *testing.B) {
 	var q = priority.New[int]()
 
+	var m = make(map[int]int64)
+	var r = rand.NewSource(time.Now().Unix())
 	for i := 0; i < b.N; i++ {
-		q.Enqueue(i, int64(i))
+		m[i] = r.Int63()
 	}
 
-	b.Log(b.N)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		q.Enqueue(i, m[i])
+	}
 }
